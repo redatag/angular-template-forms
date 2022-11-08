1c896680ac90b429a58404195c208624c2d85e3a
Reda Tag <rtag@ncgr.gov.sa>
2022-11-08 18:40:27 +0300

    Add template form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-forms';
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = new User ('Reda', 'ss@fd.com',123456,'Avnguler', '5 pm',true);
 }
